2e65a548438a3cea65c03f4374d58692c8beaf9d
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-19 19:49:42 +0200

    Adding custom validation message

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,14 +1,13 @@
 class Friendship < ApplicationRecord
-    belongs_to :user
-    belongs_to :friend, class_name: :User
+  belongs_to :user
+  belongs_to :friend, class_name: :User
 
-    validates :user_id, uniqueness: { scope: :friend_id }
-    validate :cannot_add_self
+  validates :user_id, uniqueness: { scope: :friend_id, message: "You already have this user as a friend" }
+  validate :cannot_add_self
 
-    private
+  private
 
-    def cannot_add_self
-        errors.add(:user_id, 'You cannot add yourself as a friend.') if user_id == friend_id
-    end
-    
+  def cannot_add_self
+    errors.add(:user_id, "You cannot add yourself as a friend.") if user_id == friend_id
+  end
 end
