6d22e43c0027bf9e02978d3d108e28d15a4bd85a
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-19 11:46:15 +0200

    Fixed Group validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,13 +5,9 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :users
 
   validates :name, presence: true, uniqueness: true
-  validate :cannot_add_self, :members_uniqness, :friends_only
+  validate :members_uniqness, :friends_only
 
   private
-
-  def cannot_add_self
-    errors.add(:owner, "You cannot add yourself to your own group.") if user_ids[-1] == owner_id
-  end
 
   def members_uniqness
     errors.add(:user, "You cannot repeat a member in a group.") unless user_ids.uniq == user_ids
@@ -20,8 +16,11 @@ class Group < ApplicationRecord
   def friends_only
     owner = User.find owner_id
     user_ids.each { |user_id|
-      unless user_id == owner_id
-        errors.add(:user, "#{User.find(user_id).full_name} is not in your friend list") unless owner.friend_with? user_id
+      if user_id == owner_id
+        errors.add(:owner, "You cannot add yourself to your own group.")
+      else
+        user = User.find user_id
+        errors.add(:user, "##{user.id} #{user.full_name} is not in your friend list") unless owner.friend_with? user_id
       end
     }
   end
