843c7e03fd61090a323a25b6dea4bf6fc74ec8a1
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-19 16:16:24 +0200

    Fixed friends_controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -6,14 +6,12 @@ class FriendsController < ApplicationController
   def create
     candidate_friend = User.find_by email: params[:email]
     if candidate_friend
-      if candidate_friend.empty?
-        flash[:info] = "Empty input"
-      elsif Friendship.create friend_id: candidate_friend.id, user_id: current_user.id 
+      if Friendship.create friend_id: candidate_friend.id, user_id: current_user.id
         flash[:info] = "#{candidate_friend.full_name} is added"
       else
         flash[:error] = "Unable to add friend"
       end
-    else 
+    else
       flash[:error] = "User doesn't exist"
     end
     redirect_to friends_path
