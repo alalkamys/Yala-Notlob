b78f065d1c036dd37fbcfe14129f313efcb182d8
Mostafa-Moawad <mostafa.moawad2211@gmail.com>
2021-04-20 02:55:17 +0200

    user can list, add and delete groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,27 @@
 Rails.application.routes.draw do
 
-  # * Friends controller routes without using resources
+  # get 'groups/index'
+  # get 'groups/new'
+  # get 'groups/create'
+  # get 'groups/destroy'
+  # get 'groups/add_user'
+  # get 'groups/create_user'
+  # get 'groups/destroy_user'
+
   
+  # * Friends controller routes without using resources
+
   # get "/friends/", to: "friends#index", as: "friends"
   # post "/friends/", to: "friends#create"
   # delete "/friends/:id", to: "friends#destroy", as: "friend"
 
 
   resources :friends
+  resources :groups do
+    get '/user', to: 'groups#add_user', as: 'add_user'
+    post '/user', to:	'groups#create_user', as: 'create_user'
+    delete '/user/:user_id', to: 'groups#destroy_user', as: 'destroy_user'
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
