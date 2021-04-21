81bae3d646b44b55b39cd6addba6ad11653a59f9
ebrahimmagdy <ebrahim.magdy47@gmail.com>
2021-04-21 20:17:14 +0200

    creat orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,31 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.where(user_id: current_user.id)
+    # @orders = Order.where(user_id: current_user.id)
+    @orders = Order.all
   end
 
   def show
     @order = Order.find(params[:id])
+    @Items = OrderMember.joins(:order)
   end
 
   def new
+    @order = Order.new
+    @friends = Friendship.where(user_id: current_user.id)
   end
 
   def create
+    @order = Order.new(order_params)
+    @friends = Friendship.where(user_id: current_user.id)
+    if @order.save
+      redirect_to @order
+    else
+      render :new
+    end
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:mealtype, :resturant_name, :menu_img).merge(user: current_user)
+    end
 end
