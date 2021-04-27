b7d2d458a1182c2247024db2e0934a45f9e356b7
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-27 23:45:27 +0200

    Testing notification when adding a new friend

--- app/models/invited_member.rb
+++ app/models/invited_member.rb
@@ -1,6 +1,6 @@
 class InvitedMember < ApplicationRecord
   belongs_to :order
-  belongs_to :user 
+  belongs_to :user
 
   validates :user_id, uniqueness: { scope: :user_id, message: "You already have this user invited" }
   validate :cannot_add_self
@@ -11,10 +11,11 @@ class InvitedMember < ApplicationRecord
 
   # check if he is a friend or not
   def inFriend?(user_id)
-    self.order.user.friends.where(id:user_id)
+    self.order.user.friends.where(id: user_id)
   end
 
   private
+
   def cannot_add_self
     errors.add(:user_id, " you cannot Invite yourself.") if user_id == order.user_id
   end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -8,12 +8,35 @@ class Notification < ApplicationRecord
   scope :unread, -> { where(read_at: nil) }
   scope :unviewed, -> { where(recieved: false) }
 
-  def self.notify_invite(invited_member)
+  # def self.notify_invite(invited_member)
+  #   new_notification = Notification.new
+  #   new_notification.receiver_id = invited_member.user_id
+  #   new_notification.notification_type = 0
+  #   new_notification.order_id = invited_member.order_id
+  #   new_notification.sender_id = invited_member.order.user.id
+  #   new_notification.viewed = false
+  #   new_notification.save()
+  #   data = {
+  #     :notification_id => new_notification.id,
+  #     :receiver => new_notification.receiver,
+  #     :notification_type => new_notification.notification_type,
+  #     :order => new_notification.order,
+  #     :sender => new_notification.sender,
+  #     :viewed => new_notification.viewed,
+  #   }
+  #   puts "=============Invitation Data==================="
+  #   puts data
+  #   puts "==============================================="
+
+  #   ActionCable.server.broadcast("notification_channel_#{new_notification.receiver_id}", data)
+  # end
+
+  def self.notify_invite()
     new_notification = Notification.new
-    new_notification.receiver_id = invited_member.user_id
+    new_notification.receiver_id = 2
     new_notification.notification_type = 0
-    new_notification.order_id = invited_member.order_id
-    new_notification.sender_id = invited_member.order.user.id
+    new_notification.order_id = 1
+    new_notification.sender_id = 1
     new_notification.viewed = false
     new_notification.save()
     data = {
@@ -54,7 +77,6 @@ class Notification < ApplicationRecord
     ActionCable.server.broadcast("notification_channel_#{new_notification.receiver_id}", data)
   end
 
-  
-    ActionCable.server.broadcast("notification_channel_#{new_notification.receiver_id}", data)
-  end
+  #   ActionCable.server.broadcast("notification_channel_#{new_notification.receiver_id}", data)
+  # end
 end
